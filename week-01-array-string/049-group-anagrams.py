a36e62980d971c147f67816b116891270fd13aec
Nguyen The Lam <nguyenthelam27072007tb@gmail.com>
2026-04-07 14:15:25 +0700

    049

--- week-01-array-string/049-group-anagrams.py
+++ week-01-array-string/049-group-anagrams.py
@@ -1,2 +1,9 @@
 def groupAnagrams(strs: list[str]) -> list[list[str]]:
-    
+    d = {}
+    for i in strs:
+        sorted_str = ''.join(sorted(i))
+        if sorted_str in d:
+            d[sorted_str].append(i)
+        else:
+            d[sorted_str] = [i]
+    return list(d.values())
